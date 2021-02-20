7bd158c814fc8a1454fdd9ecbeca6cdd7ee7a9da
student <jerrytd579@gmail.com>
2021-02-20 13:45:11 -0500

    updated jerry_syscall 1 to use copy to and copy from user

--- cs492_2021s_assignment1/syscall1/jerry_syscall.c
+++ cs492_2021s_assignment1/syscall1/jerry_syscall.c
@@ -1,20 +1,27 @@
 #include <linux/kernel.h>
 #include <linux/string.h>
+#include <linux/syscalls.h>
+#include <asm/uaccess.h>
 
 asmlinkage long jerry_syscall(char input[]){
 	int count = 0;
 	int i;
+	char buf[128];
+	copy_from_user(buf, input, sizeof(buf));
 
-	if (strlen(input) > 128){
+
+	if (strlen(buf) > 128){
 		return -1;
 	}
 
-	for(i = 0; i < strlen(input); i++){
-		if(input[i] == 'x'){
-			input[i] = 'y';
+	for(i = 0; i < strlen(buf); i++){
+		if(buf[i] == 'x'){
+			buf[i] = 'y';
 			count += 1;
 		}
 	}
 
+	copy_to_user(input, buf, sizeof(buf));
+	printk(KERN_INFO "This is the system call: Pid: %d", current->pid);
 	return count;
 }
